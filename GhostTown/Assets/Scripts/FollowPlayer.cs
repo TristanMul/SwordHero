27bf8077ba90f358320abf08c09a7c3bf00f6522
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-04 16:29:37 +0100

    Made a follow player script
    Made a follow player script all enemies will be able to use.

---
+++ GhostTown/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public EnemyBaseClass controllerClass;  // The controller class of this enemy object.
+    private GameObject player;
+    private NavMeshAgent navMeshAgent;
+    
+
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = controllerClass.speed;
+    }
+
+    private void FixedUpdate() {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer(){
+        if(player && controllerClass.enemyState == EnemyBaseClass.EnemyState.Move){
+            transform.LookAt(player.transform);
+            navMeshAgent.SetDestination(player.transform.position);
+        }
+    }
+
+}

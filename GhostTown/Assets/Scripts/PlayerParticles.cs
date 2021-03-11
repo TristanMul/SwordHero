babfde5eb9e9035572e176d734a15ebaa45f9e5c
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-03-11 16:45:59 +0100

    DeathParticles getest

--- GhostTown/Assets/Scripts/PlayerParticles.cs
+++ GhostTown/Assets/Scripts/PlayerParticles.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleTest : MonoBehaviour
+public class PlayerParticles: MonoBehaviour
 {
     public bool testing;
     bool particlesRunning;//To activate the charging particles set ParticlesRunning to true, mind the capitol letter
@@ -14,7 +14,6 @@ public class ParticleTest : MonoBehaviour
         get { return particlesRunning; }
         set
         {
-            Debug.Log(value);
             if (value) { 
                 chargingParticles.Play();
             }
@@ -34,10 +33,17 @@ public class ParticleTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            if (testing && Input.GetKeyDown(KeyCode.Mouse0))
+        if (testing)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 ParticlesRunning = !particlesRunning;
             }
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                PlayIsCharged();
+            }
+        }       
     }
 
     public void PlayIsCharged()

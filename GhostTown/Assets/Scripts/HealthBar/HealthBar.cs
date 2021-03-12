d1f2e193b9767b0ef5efa7632e3da3ba4c4ae461
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-12 11:15:12 +0100

    Health bar only shows when not on full health

--- GhostTown/Assets/Scripts/HealthBar/HealthBar.cs
+++ GhostTown/Assets/Scripts/HealthBar/HealthBar.cs
@@ -5,11 +5,21 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Image fillImage;
+    [SerializeField] private Image fillImage;
+    [SerializeField] private Gradient gradient;
 
     // Update the healthbar to show current health percentage. 
     public void UpdateHealthBar(float currentHealth, float maxHealth){
-        fillImage.fillAmount = currentHealth / maxHealth;
-    }
+        float healthPercentage = currentHealth / maxHealth;
 
+        if(healthPercentage == 1f){
+            GetComponent<Canvas>().enabled = false;
+        }
+        else{
+            GetComponent<Canvas>().enabled = true;
+            fillImage.fillAmount = healthPercentage;
+            fillImage.color = gradient.Evaluate(healthPercentage);
+        }
+        
+    }
 }

b56af27cf6fef31c02f890a62ee2847985838a06
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-16 14:54:22 +0100

    Update PlayerHealth.cs
    Player is invulnerable after taking damage.
    Player can take damage from enemies.
    Player flashes after taking damage.

--- GhostTown/Assets/Scripts/Player/PlayerHealth.cs
+++ GhostTown/Assets/Scripts/Player/PlayerHealth.cs
@@ -8,6 +8,7 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float maxHealth;
     private HealthBar healthBar;
     private Animator animator;
+    private bool invulnerable;
 
     void Start()
     {
@@ -16,16 +17,53 @@ public class PlayerHealth : MonoBehaviour
         healthBar.UpdateHealthBar(currentHealth, maxHealth);
 
         animator = transform.Find("Archer").GetComponent<Animator>();
+
+        invulnerable = false;
     }
 
     public void TakeDamage(float damage){
-        // Deal damage to player.
-        currentHealth -= damage;
-        healthBar.UpdateHealthBar(currentHealth, maxHealth);
+        if(!invulnerable){
+            // Player can't take damage for a while after taking damage.
+            invulnerable = true;
 
-        if(currentHealth <= 0){
-            // Player death logic here.
-            // animator.Play("Death");
+            // Play flashing effect.
+            StartCoroutine(DamagedEffect());
+
+            // Deal damage to player.
+            currentHealth -= damage;
+            healthBar.UpdateHealthBar(currentHealth, maxHealth);
+
+            // Check if player is dead.
+            if(currentHealth <= 0){
+                // Player death logic here.
+                // animator.Play("Death");
+            }
+        }
+    }
+
+    IEnumerator DamagedEffect(){
+        int flashDuration = 5;   // The amount of times renderer should be turned off and on again.
+        int flashCount = 0;     //  Counter for flashing.
+
+        // Turn renederer off and on again for flashing effect.
+        while (flashCount < flashDuration)
+        {
+            transform.Find("Archer").gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+            transform.Find("Archer").gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+
+            flashCount++;
+        }
+
+        // Player can take damage again.
+        invulnerable = false;
+    }
+
+    private void OnCollisionStay(Collision other) {
+        // Player got hit by enemy.
+        if(other.gameObject.tag == "Enemy"){
+            TakeDamage(2f);
         }
     }
 

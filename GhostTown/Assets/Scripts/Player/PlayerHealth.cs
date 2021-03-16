3a983a9e2b2ede5e874c519e067eddb5576cadd2
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-16 11:17:00 +0100

    Update PlayerHealth.cs
    Already put death animation in player health script.
    Just need to uncomment "animator.Play("Death");" after pulling animation.

--- GhostTown/Assets/Scripts/Player/PlayerHealth.cs
+++ GhostTown/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,12 +7,15 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float currentHealth;
     [SerializeField] private float maxHealth;
     private HealthBar healthBar;
+    private Animator animator;
 
     void Start()
     {
         // Get player healthbar.
         healthBar = transform.Find("HealthBar").GetComponent<HealthBar>();
         healthBar.UpdateHealthBar(currentHealth, maxHealth);
+
+        animator = transform.Find("Archer").GetComponent<Animator>();
     }
 
     public void TakeDamage(float damage){
@@ -22,6 +25,7 @@ public class PlayerHealth : MonoBehaviour
 
         if(currentHealth <= 0){
             // Player death logic here.
+            // animator.Play("Death");
         }
     }
 

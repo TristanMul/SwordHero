83c38d9d291a7d758aca694a110187bc4a6a5fe4
Tristan <tristanmul@gmail.com>
2021-03-17 17:10:28 +0100

    Trying to make spider attack only once

--- GhostTown/Assets/Scripts/Enemies/spiderKing/SpiderKing.cs
+++ GhostTown/Assets/Scripts/Enemies/spiderKing/SpiderKing.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class SpiderKing : EnemyBaseClass
 {
+    [SerializeField] private MoveSpiderKing movement;
+    public Animator npcAnimator;
+    int hashCastSpell; 
     void Start()
     {
+        hashCastSpell = Animator.StringToHash("castSpell");
         enemyState = EnemyState.Idle;
+    }
+    private void FixedUpdate()
+    {
+        shootProjectiles();
+    }
+    IEnumerator shootProjectiles()
+    {
+        Debug.Log(npcAnimator.GetBool(movement.hashReachedDestination));
+        if (npcAnimator.GetBool(movement.hashReachedDestination))
+        {
+           yield return new WaitForSeconds(0.6f);
+            npcAnimator.SetBool(hashCastSpell, true);
+        }
     }
 }

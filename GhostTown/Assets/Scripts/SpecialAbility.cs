3f1d522b5a1c4d48e49c3bbfc6119a311831e30b
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-03-17 16:55:05 +0100

    Special Attack scales right

--- GhostTown/Assets/Scripts/SpecialAbility.cs
+++ GhostTown/Assets/Scripts/SpecialAbility.cs
@@ -6,29 +6,38 @@ public class SpecialAbility : MonoBehaviour
 {
    
     [SerializeField] private float increaseSize;
-    private float sizeIncreased = 0;
-    Vector3 circleResetSize;
     [SerializeField] private float triggerSize;
+    private float sizeIncreased = 0;
+    private Transform rangeRing;
+    private ArrowRing arrow;
+
     [HideInInspector] public bool powerCharged = false;
     public Color translucentYellow;
     public Color translucentWhite;
     public PlayerParticles particles;
+
     PlayerMovement player;
     ArrowRing arrowRing;
-
+    Vector3 circleResetSize;
+    DestroyAfterSeconds arrowRange;
 
     private void Awake()
     {
+        arrow = GameObject.Find("Special Attack").GetComponent<ArrowRing>();
+        arrowRange = arrow.arrow.GetComponent<DestroyAfterSeconds>();
         player = GetComponentInParent<PlayerMovement>();
         arrowRing = player.GetComponentInChildren<ArrowRing>();
         sizeIncreased += transform.localScale.x;
         circleResetSize = transform.localScale;
+        rangeRing = GameObject.Find("Arrow Range").GetComponent<Transform>();
     }
 
     private void FixedUpdate()
     {
-        
+        arrowRange.timer = triggerSize / 10f;
+        rangeRing.localScale = new Vector3(triggerSize / 1.22f, triggerSize / 1.22f, 0);
     }
+
     public void ChargePower()
     {
         if (player.movement.x != 0 && player.movement.z != 0)

0931590dd81a8e78e516b458868f4d2614cc9db1
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-17 13:16:34 +0100

    Update Camera logic
    CameraManager gets reference to player from GameManager.

--- GhostTown/Assets/Scripts/CameraManager.cs
+++ GhostTown/Assets/Scripts/CameraManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform FollowTarget;
-    public Vector3 TargetOffset;
-    public float MoveSpeed = 3f;
-
-    private Transform _myTransform;
+    private Transform FollowTarget;
+    [SerializeField] private Vector3 TargetOffset;
+    [SerializeField] private float MoveSpeed = 3f;
 
     private void Start()
     {
-        _myTransform = transform;
+        FollowTarget = GameManager.instance._player.transform;
     }
 
     public void SetTarget(Transform aTransform)
@@ -23,6 +21,6 @@ public class CameraManager : MonoBehaviour
     private void LateUpdate()
     {
         if (FollowTarget != null)
-            _myTransform.position = Vector3.Lerp(_myTransform.position, FollowTarget.position + TargetOffset, MoveSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, FollowTarget.position + TargetOffset, MoveSpeed * Time.deltaTime);
     }
 }

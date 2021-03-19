5a34effa3b00b632fb83c3ff78ceb7ebf3a8c3cc
Tristan <tristanmul@gmail.com>
2021-03-19 15:13:16 +0100

    Added spider bomb AoE

--- GhostTown/Assets/explode.cs
+++ GhostTown/Assets/explode.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class explode : MonoBehaviour
-{
-    [SerializeField] private ParticleSystem explosion2;
+{ 
     public GameObject explosion;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+ 
 
     private void OnTriggerEnter(Collider other)
     {

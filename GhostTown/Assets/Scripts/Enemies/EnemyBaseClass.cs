f274a25e46a9c58156b3c93e1bb472db03be5e1f
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-03-19 16:03:39 +0100

    BossSpider emote working

--- GhostTown/Assets/Scripts/Enemies/EnemyBaseClass.cs
+++ GhostTown/Assets/Scripts/Enemies/EnemyBaseClass.cs
@@ -27,6 +27,7 @@ public class EnemyBaseClass : MonoBehaviour
         Damaged,
         Death,
         SpecialAttack,
+        Emote, 
     }
     protected string idleAnimation;
     protected string moveAnimation;
@@ -34,6 +35,7 @@ public class EnemyBaseClass : MonoBehaviour
     protected string damagedAnimation;
     protected string deathAnimation;
     protected string specialAttackAnimation;
+    protected string emoteAnimation;
 
     public string IdleAnimation { get { return idleAnimation; } protected set { idleAnimation = value; } }
     public string MoveAnimation { get { return moveAnimation; } protected set {  moveAnimation = value; } }
@@ -41,6 +43,7 @@ public class EnemyBaseClass : MonoBehaviour
     public string DamagedAnimation { get { return damagedAnimation; } protected set {  damagedAnimation = value; } }
     public string DeathAnimation { get { return deathAnimation; } protected set {  deathAnimation = value; } }
     public string SpecialAttackAnimation { get { return specialAttackAnimation; } protected set { specialAttackAnimation = value; } }
+    public string EmoteAnimation { get { return emoteAnimation; } protected set { emoteAnimation = value; } }
 
     private void Awake() {
         // Get healthbar for this enemy.
--- GhostTown/Assets/Scripts/Enemies/EnemyTrigger/TriggerSpiderKing.cs
+++ GhostTown/Assets/Scripts/Enemies/EnemyTrigger/TriggerSpiderKing.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class TriggerSpiderKing : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         // Activate when player enters range.
-        if(other.tag == "Player"){
+        if (other.tag == "Player")
+        {
             foreach (Transform enemy in transform)
             {
+                bool isKing = false;
                 // Hide range indicator.
-                if(enemy.name == "AllAxis_Outline" || enemy.name == "Ring Mesh"){
+                if (enemy.name == "AllAxis_Outline" || enemy.name == "Ring Mesh")
+                {
                     enemy.gameObject.SetActive(false);
                 }
-
+                if (enemy.name == "Spider King")
+                {
+                    enemy.GetComponent<SpiderKing>().Drop();
+                    GetComponent<Collider>().enabled = false;
+                    isKing = true;
+                }
                 // All enemies start attacking the player.
-                if(enemy.GetComponent<EnemyBaseClass>()){
+                if (enemy.GetComponent<EnemyBaseClass>() && !isKing)
+                {
                     // Turn off collider so trigger won't trigger multiple times.
                     GetComponent<Collider>().enabled = false;
 
@@ -24,6 +34,7 @@ public class TriggerSpiderKing : MonoBehaviour
                     enemy.GetComponent<SpiderKing>().enabled = true;
                     enemy.GetComponent<EnemyBaseClass>().enemyState = EnemyBaseClass.EnemyState.Move;
                 }
+
             }
         }
     }

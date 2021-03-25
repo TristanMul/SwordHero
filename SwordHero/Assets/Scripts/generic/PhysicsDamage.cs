addaa17c65f8977e81c430221b66bd3c13476878
Tristan <tristanmul@gmail.com>
2021-03-25 13:53:23 +0100

    box collides with enemy

--- SwordHero/Assets/Scripts/generic/PhysicsDamage.cs
+++ SwordHero/Assets/Scripts/generic/PhysicsDamage.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class PhysicsDamage : MonoBehaviour
 {
     Rigidbody rb;
-
+    [SerializeField] private float damageMultiplier = 1.5f;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (other.GetComponent<EnemyHealth>())
+            {
+                other.GetComponent<EnemyHealth>().TakeDamage(rb.velocity.magnitude * damageMultiplier);
+                
+            }
+        }
     }
 }

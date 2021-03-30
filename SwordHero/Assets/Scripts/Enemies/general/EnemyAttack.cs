ee4c5836db44e35b2a7862164eb46e23c79bca12
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-03-30 13:22:46 +0200

    Golem attack temporary fix
    Golem can attack and move again.

--- SwordHero/Assets/Scripts/Enemies/general/EnemyAttack.cs
+++ SwordHero/Assets/Scripts/Enemies/general/EnemyAttack.cs
@@ -6,19 +6,40 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     public EnemyBaseClass controllerClass;  // The controller class of this enemy object.
+    private Animator animator;
     private GameObject player;
+    private bool isAttacking;
+    private float attackTimer;
     
     void Start()
     {
+        animator = GetComponent<Animator>();
         player = GameManager.instance._player;
+        isAttacking = false;
+    }
+
+    private void Update() {
+        CheckAttackRange();
     }
 
     // Check if the player is in the range of this enemy's attack.
     private void CheckAttackRange(){
         // The player is in the range of this enemies attack.
-        if(Vector3.Distance(transform.position, player.transform.position) <= controllerClass.AttackRange){
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if(distance <= controllerClass.AttackRange && !isAttacking){
+            isAttacking = true;
             controllerClass.enemyState = EnemyBaseClass.EnemyState.Attack;
             GetComponent<FollowPlayer>().enabled = false;
+
+            attackTimer = animator.GetCurrentAnimatorStateInfo(0).length;
+            Invoke("AttackCompleted", attackTimer);
+
         }
+    }
+
+    private void AttackCompleted(){
+        isAttacking = false;
+        controllerClass.enemyState = EnemyBaseClass.EnemyState.Move;
+        GetComponent<FollowPlayer>().enabled = true;
     }
 }

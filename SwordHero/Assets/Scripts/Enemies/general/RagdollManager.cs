1d96ef657ac2020cc72d40c8963fe9d23a757677
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-04-02 11:38:38 +0200

    Skeleton ragdoll when hit

--- SwordHero/Assets/Scripts/Enemies/general/RagdollManager.cs
+++ SwordHero/Assets/Scripts/Enemies/general/RagdollManager.cs
@@ -12,10 +12,17 @@ public class RagdollManager : MonoBehaviour
     Rigidbody rb;
     EnemyHealth health;
 
+    [Header("fall variables")]
+    #region variablesForFalling
+    [SerializeField] float startRotationSpeed;
+    float currentRotationSpeed;
+    [SerializeField] float rotationAcceleration;
+    bool fallRotationStarted;
+    [SerializeField] float timeToStandUp;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(transform.rotation);
         ragdollColliders = GetComponentsInChildren<CapsuleCollider>();
         physicsDamage = GetComponent<PhysicsDamage>();
         agent = GetComponent<NavMeshAgent>();
@@ -23,18 +30,25 @@ public class RagdollManager : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         health = GetComponent<EnemyHealth>();
 
+
         health.StartRagdoll += SetupRagdoll;
         health.StopRagdoll += DisableRagdoll;
         DisableRagdoll();
-
     }
 
 
-    void SetupRagdoll() {
+    void SetupRagdoll()
+    {
         animator.enabled = false;
         agent.enabled = false;
         ChangeRagdollColliders(true);
         rb.isKinematic = false;
+
+        if (!fallRotationStarted)
+        {
+            StartCoroutine(FallRotation());
+            fallRotationStarted = true;
+        }
     }
     void DisableRagdoll()
     {
@@ -49,9 +63,35 @@ public class RagdollManager : MonoBehaviour
     /// Either disables or enables many colliders to do with the ragdoll
     /// </summary>
     /// <param name="value">Wether to enable or disable the colliders</param>
-    void ChangeRagdollColliders(bool value) {
-        foreach (CapsuleCollider col in ragdollColliders) {
+    void ChangeRagdollColliders(bool value)
+    {
+        foreach (CapsuleCollider col in ragdollColliders)
+        {
             col.enabled = value;
-        } 
+        }
     }
+
+    IEnumerator FallRotation()
+    {
+        float distanceRotated = 0f;
+        currentRotationSpeed = startRotationSpeed;
+        Quaternion startRotation = transform.rotation;//saves the rotation at the beginning
+        while (distanceRotated < 90f)
+        {
+            transform.Rotate(new Vector3(currentRotationSpeed * Time.deltaTime, 0f, 0f));
+            distanceRotated += currentRotationSpeed * Time.deltaTime;
+            currentRotationSpeed += Time.deltaTime * rotationAcceleration;//Speeds up the falling
+            yield return null;
+        }
+        yield return new WaitForSeconds(timeToStandUp);
+        animator.enabled = true;
+        transform.rotation = startRotation;
+        animator.Play("StandUp");
+        DisableRagdoll();
+    }
+
+    public void CharacterIsStanding() {
+        fallRotationStarted = false;//Enables the ability to fall down
+    }
+
 }

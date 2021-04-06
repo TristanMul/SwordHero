da3398c8ca865bd7a0ceb1c299f7028637c3dbef
OmarOH <36475683+OmarOH@users.noreply.github.com>
2021-04-06 13:55:52 +0200

    Enemy explode on death script added
    - Skeleton of enemy explode on trigger
    - Trigger has yet to be configured

---
+++ SwordHero/Assets/Scripts/Enemies/general/ExplodeInPieces.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeInPieces : MonoBehaviour
+{
+    Transform[] parts;
+    Vector3 randomForce;
+    Transform parentObject;
+    Transform oldParent;
+    List<GameObject> childObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldParent = transform.parent;
+        parts = GetComponentsInChildren<Transform>();
+        childObjects = new List<GameObject>();
+        randomForce = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+        parentObject = transform.parent.GetComponent<Transform>();
+
+        foreach(Transform child in parts)
+        {
+            if (child != parts[0])
+            {
+                childObjects.Add(child.gameObject);
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(parts.Length);
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            SetupForDestroy();
+            AddForces(parts);
+        }
+    }
+
+    void SetupForDestroy()
+    {
+        transform.parent = null;
+        oldParent.gameObject.SetActive(false);
+    }
+
+    void AddForces(Transform[] obj)
+    {
+        foreach (GameObject rb in childObjects)
+        {
+            rb.gameObject.SetActive(true);
+            StartCoroutine(AddForce(rb));
+        }
+    }
+
+    IEnumerator AddForce(GameObject rb)
+    {
+        float duration = 0.1f;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            rb.GetComponent<Rigidbody>().AddExplosionForce(2, transform.position, 2, 1, ForceMode.Impulse);
+            yield return null;
+        }
+    }
+}
